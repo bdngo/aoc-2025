8d6b9230eda3c3f99e7aafa9ea8a4aa615abd5f7
Bryan Ngo <bryanngo@berkeley.edu>
2025-12-01 23:04:22 -0800

    finish day 2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,4 +1,7 @@
-fn is_invalid_id(id: u64) -> Option<u64> {
+use fancy_regex::Regex;
+
+#[allow(dead_code)]
+fn is_invalid_id_part1(id: u64) -> Option<u64> {
     let id_string = id.to_string();
     if !id_string.len().is_multiple_of(2) {
         None
@@ -8,6 +11,7 @@ fn is_invalid_id(id: u64) -> Option<u64> {
     }
 }
 
+#[allow(dead_code)]
 pub fn part1(input: String) -> u64 {
     let ranges: Vec<_> = input
         .trim()
@@ -21,7 +25,35 @@ pub fn part1(input: String) -> u64 {
         .collect();
     let mut invalid_id_sum: u64 = 0;
     for range in ranges {
-        invalid_id_sum += range.filter_map(|id| is_invalid_id(id)).sum::<u64>();
+        invalid_id_sum += range.filter_map(|id| is_invalid_id_part1(id)).sum::<u64>();
+    }
+    invalid_id_sum
+}
+
+fn is_invalid_id_part2(id: u64) -> Option<u64> {
+    let pat = Regex::new(r"^(\d+?)\1+$").unwrap();
+    if pat.is_match(id.to_string().as_str()).unwrap() {
+        Some(id)
+    } else {
+        None
+    }
+}
+
+pub fn part2(input: String) -> u64 {
+    let ranges: Vec<_> = input
+        .trim()
+        .split(",")
+        .map(|x| x.split_once("-").unwrap())
+        .map(|(x, y)| {
+            let parsed_x: u64 = x.parse().unwrap();
+            let parsed_y: u64 = y.parse().unwrap();
+            parsed_x..=parsed_y
+        })
+        .collect();
+    let mut invalid_id_sum: u64 = 0;
+    for range in ranges {
+        let invalid_ids = range.filter_map(|id| is_invalid_id_part2(id));
+        invalid_id_sum += invalid_ids.sum::<u64>();
     }
     invalid_id_sum
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let fname = &args[1];
     let input = fs::read_to_string(fname)?;
 
-    let solution = day02::part1(input);
+    let solution = day02::part2(input);
     println!("{}", solution);
 
     Ok(())
