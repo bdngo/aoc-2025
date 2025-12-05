44967510f9cec8cfc8af221b3432fb5ccb99ff92
Bryan Ngo <bryanngo@berkeley.edu>
2025-12-04 22:14:17 -0800

    finish set-based solution, OoM

--- src/day05.rs
+++ src/day05.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use crate::utils;
+use std::collections::BTreeSet;
 
 pub fn part1(input: String) -> u64 {
     let (id_ranges, ids) = input.split_once("\n\n").unwrap();
@@ -17,4 +18,19 @@ pub fn part1(input: String) -> u64 {
     ids.into_iter()
         .map(|x| id_ranges.iter().any(|y| y.contains(&x)) as u64)
         .sum()
+}
+
+pub fn part2(input: String) -> u64 {
+    let id_ranges = input.split_once("\n\n").unwrap().0;
+    let id_range_set = id_ranges
+        .lines()
+        .map(|x| x.split_once("-").unwrap())
+        .map(|(x, y)| {
+            let parsed_x: u64 = x.parse().unwrap();
+            let parsed_y: u64 = y.parse().unwrap();
+            (parsed_x..=parsed_y).collect::<BTreeSet<_>>()
+        })
+        .reduce(|acc, e| acc.union(&e).cloned().collect())
+        .unwrap();
+    id_range_set.len() as u64
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let fname = &args[1];
     let input = fs::read_to_string(fname)?;
 
-    let solution = day05::part1(input);
+    let solution = day05::part2(input);
     println!("{}", solution);
 
     Ok(())
